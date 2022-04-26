9d2d0c8b25422270bb5f846ba0bff790ed3bf8aa
Oleksii Persatyi <persatyi@ukr.net>
2022-04-26 21:12:49 +0300

    removed superluous console logs

--- src/components/Views/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/Views/MovieDetailsPage/MovieDetailsPage.jsx
@@ -24,11 +24,8 @@ const MovieDetailsPage = () => {
   const { url } = useRouteMatch();
   const { movieId } = useParams();
   const location = useLocation();
-  console.log('🚀 ~ location', location);
   const history = useHistory();
-  console.log('🚀 ~ history', history);
   const [movie, setMovie] = useState(null);
-  console.log('🚀 ~ movie', movie);
 
   useEffect(() => {
     if (movieId) {
